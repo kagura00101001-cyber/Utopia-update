ef89b4d4c4d7c96d7b4e1b7edffd6a0dcdc69ae0
kagura00101001-cyber <kagura00101001@gmail.com>
2026-08-13 11:34:08 +0800

    Add Ozon Complaint Tampermonkey update metadata

---
+++ Ozon_Complaint.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Ozon 自动投诉 V1.0.0
+// @namespace    kagura.ozon.auto.complaint
+// @version      1.1.3
+// @description  Ozon 自动投诉脚本更新元数据
+// @author       Kagura
+// @updateURL    https://raw.githubusercontent.com/kagura00101001-cyber/Utopia-update/main/Ozon_Complaint.meta.js
+// @downloadURL  https://raw.githubusercontent.com/kagura00101001-cyber/Utopia-update/main/Ozon_Complaint.user.js
+// ==/UserScript==

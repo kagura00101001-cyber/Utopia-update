bc71cc238f4e7c6944c5d9a4068b2c2521505b35
kagura00101001-cyber <kagura00101001@gmail.com>
2026-08-13 10:03:44 +0800

    准备 V3.0.31 更新元数据

---
+++ release_build/3.0.31/Ozon_ChatGPT.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Ozon主图下载 + ChatGPT批量生图助手
+// @namespace    https://github.com/Kagura-userscripts
+// @version      3.0.31
+// @description  Kagura - Tampermonkey 原生手动更新元数据
+// @author       Kagura
+// @updateURL    https://raw.githubusercontent.com/kagura00101001-cyber/Utopia-update/main/Ozon_ChatGPT.meta.js
+// @downloadURL  https://raw.githubusercontent.com/kagura00101001-cyber/Utopia-update/main/Ozon_ChatGPT.user.js
+// ==/UserScript==
